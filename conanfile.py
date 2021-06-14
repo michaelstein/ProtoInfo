dbc739fea10db2b29ad3791847879b55687a871b
Michael Stein <m.stein@h3-ms.com>
2021-06-14 15:21:23 +0200

    Update dependencies.

--- conanfile.py
+++ conanfile.py
@@ -4,79 +4,55 @@ import os, time, shutil
 
 class ProtoInfoConan(ConanFile):
 	settings = "os", "compiler", "build_type", "arch"
-	generators = "cmake", "virtualenv"
+	generators = "cmake", "virtualrunenv", "virtualenv", "virtualbuildenv"
 	
 	build_requires = (
-		"cmake_installer/3.12.1@conan/stable"
+		"cmake_installer/3.15.5@conan/stable",
+		"protoc_installer/3.9.1@bincrafters/stable"
 	)
 	
 	requires = (
-		"gtest/1.8.1@bincrafters/stable",
+		"gtest/1.10.0",
 		"humblelogging/3.0.3@insanefactory/stable",
-		"protobuf/3.6.1@bincrafters/stable",
-		"Qt/5.11.2@bincrafters/stable",
+		"protobuf/3.9.1",
+		"qt/5.13.2@bincrafters/stable",
 		"gsl_microsoft/2.0.0@bincrafters/stable",
 
-		"lzma/5.2.4@bincrafters/stable",
-		"OpenSSL/1.0.2o@conan/stable"
+		"openssl/1.0.2t",
 	)
 	
 	default_options = {
 		"protobuf:shared": False,
-		"OpenSSL:shared": True,
-		"OpenSSL:no_zlib": True,
-		"Qt:shared": True,
-		"Qt:commercial": False,
-		"Qt:opengl": "desktop",
-		"Qt:openssl": True,
-		"Qt:GUI": True,
-		"Qt:widgets": True,
-		"Qt:qtbase": True,
-		"Qt:qtsvg": True,
-		"Qt:qtdeclarative": False,
-		"Qt:qtactiveqt": False,
-		"Qt:qtscript": False,
-		"Qt:qtmultimedia": True,
-		"Qt:qttools": True,
-		"Qt:qtxmlpatterns": False,
-		"Qt:qttranslations": False,
-		"Qt:qtdoc": False,
-		"Qt:qtrepotools": False,
-		"Qt:qtqa": False,
-		"Qt:qtlocation": False,
-		"Qt:qtsensors": False,
-		"Qt:qtconnectivity": False,
-		"Qt:qtwayland": False,
-		"Qt:qt3d": False,
-		"Qt:qtimageformats": True,
-		"Qt:qtgraphicaleffects": False,
-		"Qt:qtquickcontrols": False,
-		"Qt:qtserialbus": False,
-		"Qt:qtserialport": False,
-		"Qt:qtx11extras": False,
-		"Qt:qtmacextras": False,
-		"Qt:qtwinextras": False,
-		"Qt:qtandroidextras": False,
-		"Qt:qtwebsockets": False,
-		"Qt:qtwebchannel": False,
-		"Qt:qtwebengine": False,
-		"Qt:qtcanvas3d": False,
-		"Qt:qtwebview": False,
-		"Qt:qtquickcontrols2": False,
-		"Qt:qtpurchasing": False,
-		"Qt:qtcharts": False,
-		"Qt:qtdatavis3d": False,
-		"Qt:qtvirtualkeyboard": False,
-		"Qt:qtgamepad": False,
-		"Qt:qtscxml": False,
-		"Qt:qtspeech": False,
-		"Qt:qtnetworkauth": False,
-		"Qt:qtremoteobjects": False,
-		"Qt:qtwebglplugin": False,
+		"qt:shared": True,
+		"qt:commercial": False,
+		"qt:opengl": "desktop",
+		"qt:openssl": True,
+		"qt:qtcharts": True,
+		"qt:qtconnectivity": True,
+		"qt:qtgraphicaleffects": True,
+		"qt:qtimageformats": True,
+		"qt:qtlocation": True,
+		"qt:qtmultimedia": True,
+		"qt:qtnetworkauth": True,
+		"qt:qtquickcontrols2": True,
+		"qt:qtserialbus": True,
+		"qt:qtserialport": True,
+		"qt:qtspeech": True,
+		"qt:qttranslations": True,
+		"qt:qtvirtualkeyboard": True,
+		"qt:with_sqlite3": True,
+		"qt:with_mysql": False,
+		"qt:with_pq": True,
+		"qt:with_odbc": False,
+		"qt:with_openal": False,
 		"glib:shared": True,
 		"zlib:shared": True,
-		"lqr:shared": True
+		"lqr:shared": True,
 	}
+
+	def config_options(self):
+		if self.settings.os == "Windows":
+			self.options["qt"].with_harfbuzz = False
 
 	def imports(self):
 		self.copy("*.so.*", dst="bin", src="lib")

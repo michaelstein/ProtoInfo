f21fe49adf23ab79dc885e06e88758a6282367c3
mstein <michael@insanefactory.com>
2022-04-14 22:52:10 +0200

    Update requirements including Qt6

--- conanfile.py
+++ conanfile.py
@@ -1,58 +1,34 @@
 from conans import ConanFile, CMake, tools
-import os, time, shutil
+import os
 
 
 class ProtoInfoConan(ConanFile):
 	settings = "os", "compiler", "build_type", "arch"
-	generators = "cmake", "virtualrunenv", "virtualenv", "virtualbuildenv"
+	generators = "cmake", "cmake_find_package", "virtualenv", "virtualrunenv", "virtualbuildenv"
 	
 	build_requires = (
-		"cmake_installer/3.15.5@conan/stable",
-		"protoc_installer/3.9.1@bincrafters/stable"
+		"cmake/3.22.3"
 	)
 	
 	requires = (
-		"gtest/1.10.0",
+		"gtest/cci.20210126",
 		"humblelogging/3.0.3@insanefactory/stable",
-		"protobuf/3.9.1",
-		"qt/5.13.2@bincrafters/stable",
-		"gsl_microsoft/2.0.0@bincrafters/stable",
+		"protobuf/3.19.2",
+		"qt/6.2.3",
+		"ms-gsl/4.0.0",
 
-		"openssl/1.0.2t",
+		"zlib/1.2.12"
 	)
 	
 	default_options = {
 		"protobuf:shared": False,
 		"qt:shared": True,
-		"qt:commercial": False,
-		"qt:opengl": "desktop",
-		"qt:openssl": True,
-		"qt:qtcharts": True,
-		"qt:qtconnectivity": True,
-		"qt:qtgraphicaleffects": True,
-		"qt:qtimageformats": True,
-		"qt:qtlocation": True,
-		"qt:qtmultimedia": True,
-		"qt:qtnetworkauth": True,
-		"qt:qtquickcontrols2": True,
-		"qt:qtserialbus": True,
-		"qt:qtserialport": True,
-		"qt:qtspeech": True,
-		"qt:qttranslations": True,
-		"qt:qtvirtualkeyboard": True,
-		"qt:with_sqlite3": True,
-		"qt:with_mysql": False,
-		"qt:with_pq": True,
-		"qt:with_odbc": False,
-		"qt:with_openal": False,
-		"glib:shared": True,
-		"zlib:shared": True,
-		"lqr:shared": True,
+		"qt:opengl": "desktop"
 	}
 
-	def config_options(self):
-		if self.settings.os == "Windows":
-			self.options["qt"].with_harfbuzz = False
+	#def config_options(self):
+	#	if self.settings.os == "Windows":
+	#		self.options["qt"].with_harfbuzz = False
 
 	def imports(self):
 		self.copy("*.so.*", dst="bin", src="lib")
@@ -60,5 +36,5 @@ class ProtoInfoConan(ConanFile):
 		self.copy("*.dll", dst="bin", src="bin")
 		self.copy("*.dylib*", dst="bin", src="lib")
 		
-		self.copy("*", dst="bin/plugins", src="plugins", root_package="Qt")
+		self.copy("*", dst="bin/plugins", src=os.path.join("res", "archdatadir", "plugins"), root_package="qt")
 		self.copy("google/protobuf/*.proto", dst="bin/protobuf", src="include", root_package="protobuf")
---
+++ vs2019.bat
@@ -0,0 +1,2 @@
+call _build/activate.bat
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
